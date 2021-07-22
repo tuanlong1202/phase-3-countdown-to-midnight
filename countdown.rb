35a6a19ccabcafe0e3f7094746eb89ccda85ec90
tuanlong1202 <tuanlong1202@gmail.com>
2021-07-22 18:06:58 -0400

    Phase 3 countdown to midnight

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
 
-def countdown
+def countdown(number)
+    while number > 0
+        puts "#{number} SECOND(S)!"
+        number -= 1
+        countdown_with_sleep(1)
+    end
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(second)
+    sleep(second)
 end
